c6f599bd90a295b7bde974d9714787ab8799938d
Subash Neupane <subashneupane@WVM-110.claflin.edu>
2025-03-31 04:15:27 -0400

    updated Environment Variable

--- backend/config/config.py
+++ backend/config/config.py
@@ -1,7 +1,11 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+
 class Config:
-    # Directly pass the database URI
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@localhost/ecommerce'  # Set the actual database URL directly
+    SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = 'America2022@'  # Directly set your secret key
-    # Email Configuration for Flask-Mail
+    JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY")
+
    
